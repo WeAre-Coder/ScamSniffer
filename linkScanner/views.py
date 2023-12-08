f37b466a60a95e1c15b288ec60f7201981173f05
VipulPinto <VIPUL.20201CSE0596@presidencyuniversity.in>
2023-12-08 20:52:03 +0530

    Changed colours and added form

--- linkScanner/views.py
+++ linkScanner/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        link = request.POST.get('link','')
     return render(request, 'linkScanner/index.html')
 
 def learn(request):
-    return render(request, 'linkScanner/learn.html', {'name':'Learn'})
+    return render(request, 'linkScanner/learn.html')
 
 def report(request):
     return render(request, 'linkScanner/report.html')
