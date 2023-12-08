3b6eff00793c3779f2b330e722389ab2c850bcd7
DhoneChetana <149798807+DhoneChetana@users.noreply.github.com>
2023-12-08 13:13:47 +0530

    Update views.py

--- linkScanner/views.py
+++ linkScanner/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def learn():
-    pass
+def home(request):
+    return HttpResponse('<h1>WELCOME TO OUR PAGE</h1>')
 
-def report():
-    pass
+def learn(request):
+    return HttpResponse('LEARN MORE')
+
+def report(request):
+    return HttpResponse('Report if something wrong')
