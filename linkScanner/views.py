0593fcce72078eb537f0ee18249b31701765156d
dhoneChetana <dhone.20201CSE0648@presidencyuniversity.in>
2023-12-09 17:34:34 +0530

    created Report page

--- linkScanner/views.py
+++ linkScanner/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from .models import Link
+from .forms import LinkForm
 
 # Create your views here.
 def home(request):
+    link_list = Link.objects.all()
     if request.method == 'POST':
         link = request.POST.get('link','')
-    return render(request, 'linkScanner/index.html')
+        status = request.POST.get('status','')
+
+        link = Link(link=link, status=status)
+        link.save()
+        return redirect('/')
+    return render(request,'linkScanner/index.html',{'link_list':link_list})
 
 def learn(request):
     return render(request, 'linkScanner/learn.html')
